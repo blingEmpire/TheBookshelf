c78cc8db96239a1543047314e83d969be8224a17
Rachel Kim <rachelminjaekim@gmail.com>
2023-02-02 14:51:22 -0800

    worked on file structures and component management

---
+++ data/userData.sql
@@ -0,0 +1,14 @@
+create table USERS (
+	id INT,
+	firstName VARCHAR(50),
+	lastName VARCHAR(50),
+	email VARCHAR(50),
+	phoneNumber VARCHAR(50),
+	userName VARCHAR(50),
+	password VARCHAR(50)
+);
+insert into USERS (id, firstName, lastName, email, phoneNumber, userName, password) values (1, 'Chancey', 'Molesworth', 'cmolesworth0@shop-pro.jp', '909-277-4361', 'cmolesworth0', 'TUFUyOcRWqE');
+insert into USERS (id, firstName, lastName, email, phoneNumber, userName, password) values (2, 'Lynnett', 'Laffin', 'llaffin1@umn.edu', '210-893-3853', 'llaffin1', 'hNYglasQMa');
+insert into USERS (id, firstName, lastName, email, phoneNumber, userName, password) values (3, 'Kaye', 'Grishukov', 'kgrishukov2@simplemachines.org', '311-148-6098', 'kgrishukov2', 'kcyYloV');
+insert into USERS (id, firstName, lastName, email, phoneNumber, userName, password) values (4, 'Aluino', 'Kittow', 'akittow3@google.it', '377-234-3851', 'akittow3', 'N7J89R');
+insert into USERS (id, firstName, lastName, email, phoneNumber, userName, password) values (5, 'Lester', 'Blaskett', 'lblaskett4@un.org', '692-575-3139', 'lblaskett4', 'NVl4d7apd1WW');
